fa10de4993fc28cf8fa0878e5f54f09be2824029
Jules Kulcsar <kulcsar_jules@yahoo.com>
2021-11-23 14:09:42 +0200

    rgb to hex conversion

--- utils.js
+++ utils.js
@@ -338,5 +338,11 @@ const Utils = {
         const hsl1 =[h,s,l]
         return hsl1
         
+    },
+
+    rgbToHex(r, g, b) {
+        if (r > 255 || g > 255 || b > 255)
+            throw "Invalid color component";
+        return ((r << 16) | (g << 8) | b).toString(16);
     }
 };
