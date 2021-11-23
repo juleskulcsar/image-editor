46a14cc9ad6e9a23fa0b1a7472aca25f897c6cd5
Jules Kulcsar <kulcsar_jules@yahoo.com>
2021-11-23 14:10:55 +0200

    get mouse position on canvas

--- utils.js
+++ utils.js
@@ -361,5 +361,16 @@ const Utils = {
         }
         ctx.putImageData(imageData, 0, 0);
         return imageData.data;
+    },
+
+    getMousePos(canvas, event) {
+        var rect = canvas.getBoundingClientRect(), // abs. size of element
+          scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
+          scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
+    
+      return {
+        x: (event.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+        y: (event.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+      }
     }
 };
