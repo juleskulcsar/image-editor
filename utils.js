09f34dae6aa9381a291017a14e103eb2386f4d03
Jules Kulcsar <kulcsar_jules@yahoo.com>
2021-11-23 14:12:17 +0200

    adjust local brightness

--- utils.js
+++ utils.js
@@ -384,5 +384,15 @@ const Utils = {
             return { x: curleft, y: curtop };
         }
         return undefined;
+    },
+
+    localBrightness(pixels, adjustment) {
+        var d = pixels.data;
+        for (var i=0; i<d.length; i+=4) {
+          d[i] += adjustment;
+          d[i+1] += adjustment;
+          d[i+2] += adjustment;
+        }
+        return pixels;
     }
 };
