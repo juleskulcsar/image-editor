ab33c3222984d68e8e83e203f8fa67f97ded79f5
Jules Kulcsar <kulcsar_jules@yahoo.com>
2021-11-23 14:04:53 +0200

    hsl to rgb conversion

--- utils.js
+++ utils.js
@@ -1,115 +1,243 @@
 const Utils = {
-  selectImg(e) {
-    const URL = window.URL;
-    let url = URL.createObjectURL(e.target.files[0]);
-    img.src = url;
-    img.onload = function() {
-      if (img.width > 2000 || img.height > 2000) {
-        alert('image too big');
-        return;
-      }
-      // rotate(0);
-      div.appendChild(canvas);
-      draw(img);
-    };
-  },
+    selectImg(e) {
+        const URL = window.URL;
+        let url = URL.createObjectURL(e.target.files[0]);
+        img.src = url;
+        img.onload = function() {
+            if (img.width > 2000 || img.height > 2000) {
+                alert('image too big');
+                return;
+            }
+            // rotate(0);
+            div.appendChild(canvas);
+            draw(img);
+        };
+    },
 
-  editImage() {
-    let blur = document.getElementById('blur').value;
-    let brightness = document.getElementById('br').value;
-    let contrast = document.getElementById('ct').value;
-    let hue = document.getElementById('huer').value;
-    let opacity = document.getElementById('opacity').value;
-    let saturation = document.getElementById('saturation').value;
+    editImage() {
+        let blur = document.getElementById('blur').value;
+        let brightness = document.getElementById('br').value;
+        let contrast = document.getElementById('ct').value;
+        let hue = document.getElementById('huer').value;
+        let opacity = document.getElementById('opacity').value;
+        let saturation = document.getElementById('saturation').value;
 
-    filters = `blur(${blur}px) 
+        filters = `blur(${blur}px) 
   brightness(${brightness}%) 
   contrast(${contrast}%) 
   hue-rotate(${hue}deg) 
   opacity(${opacity}%)
   saturate(${saturation}%)`;
 
-    canvas.style.webkitFilter = filters;
+        canvas.style.webkitFilter = filters;
 
-    brightnessValue.innerText = brightness;
-    blurValue.innerText = blur;
-    brightnessValue.innerText = brightness;
-    contrastValue.innerText = contrast;
-    hueValue.innerText = hue;
-    opacityValue.innerText = opacity;
-    saturationValue.innerText = saturation;
-  },
+        brightnessValue.innerText = brightness;
+        blurValue.innerText = blur;
+        brightnessValue.innerText = brightness;
+        contrastValue.innerText = contrast;
+        hueValue.innerText = hue;
+        opacityValue.innerText = opacity;
+        saturationValue.innerText = saturation;
+    },
 
-  donwloadImage() {
-    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    ctx.putImageData(imageData, 0, 0);
-    let dataURL = canvas.toDataURL('image/jpeg');
-    button.href = dataURL;
-  },
+    // donwloadImage() {
+    //   let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    //   ctx.putImageData(imageData, 0, 0);
+    //   let dataURL = canvas.toDataURL('image/jpeg');
+    //   button.href = dataURL;
+    // },
+    donwloadImage() {
+        let imageCanvas = document.querySelector('.canvas');
+        let ctx = imageCanvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  rotate(degrees) {
-    //   canvas = document.querySelector('.canvas');
-    let ctx = canvas.getContext('2d');
+        // ctx.globalCompositeOperation = 'source-over';
+        draw(img);
 
-    if (degrees == 90 || degrees == 270) {
-      canvas.width = img.height;
-      canvas.height = img.width;
-    } else {
-      canvas.width = img.width;
-      canvas.height = img.height;
-    }
+        let imageCanvas2 = document.querySelector('.canvas');
+        let ctx2 = imageCanvas2.getContext('2d');
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (degrees == 90 || degrees == 270) {
-      ctx.translate(img.height / 2, img.width / 2);
-    } else {
-      ctx.translate(img.width / 2, img.height / 2);
-    }
-    ctx.rotate((degrees * Math.PI) / 180);
-    ctx.drawImage(img, -img.width / 2, -img.height / 2);
-  },
+        let textCanvas = document.querySelector('.textCanvas');
+        let ctxText = textCanvas.getContext('2d');
+        let canvasTposition = textCanvas.getBoundingClientRect();
+        let imageCanvasposition = imageCanvas.getBoundingClientRect();
+        let canvasContainer = document.querySelector('.canvas-container');
+        let canvasContainerposition = canvasContainer.getBoundingClientRect();
+        console.log('textCanvasOffset: ', textCanvas.offsetLeft);
+        console.log('imageCanvas offsetleft: ', imageCanvas.offsetLeft);
+        console.log('canvasTposition: ', canvasTposition);
+        console.log('image canvas pos: ', imageCanvasposition);
+        console.log('canvasContainerposition: ', canvasContainerposition);
+        const centerShift_x = (imageCanvas.width - image.width) / 2;
+        const centerShift_y = (imageCanvas.height - image.height) / 2;
 
-  aspectRatio(srcWidth, srcHeight, maxWidth, maxHeight) {
-    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
-    //   console.log('ratio is: ', ratio);
-    var newWidth = srcWidth * ratio;
-    var newHeight = srcHeight * ratio;
-    return {
-      width: newWidth,
-      height: newHeight
-    };
-  },
+        // console.log(
+        //     'textCanvas.width, textCanvas.height : ',
+        //     textCanvas.width,
+        //     textCanvas.height
+        // );
 
-  checkRgbSplit() {
-    if (checkRGB.checked === true) {
-      rgbButton.disabled = false;
-      rgbButton.classList.add('btn-active');
-    } else {
-      rgbButton.disabled = true;
-      rgbButton.classList.remove('btn-active');
-      draw(img);
-    }
-  },
+        // this.downloadCanvas();
 
-  checkInvertColors() {
-    if (checkInvert.checked === true) {
-      invertButton.disabled = false;
-      invertButton.classList.add('btn-active');
-    } else {
-      invertButton.disabled = true;
-      invertButton.classList.remove('btn-active');
-      draw(img);
-    }
-  },
+        ctx2.drawImage(
+            image,
+            // canvasTposition.left,
+            // canvasTposition.top,
+            // canvasTposition.width,
+            // canvasTposition.height,
+            // (canvasTposition.left - imageCanvasposition.left) - (canvasTposition.left - canvasContainerposition.left),
+            // (canvasTposition.top - imageCanvasposition.top) - (canvasTposition.top - canvasContainerposition.top),
+            // -imageCanvas.width / 2 + (canvasTposition.left - canvasContainerposition.left),
+            // -imageCanvas.height / 2 + (canvasTposition.top - canvasContainerposition.top),
+            -imageCanvas.width / 2 +
+                (canvasTposition.left - imageCanvasposition.left) +
+                60,
+            -imageCanvas.height / 2 +
+                (canvasTposition.top - imageCanvasposition.top) +
+                60,
+            imageCanvas.width,
+            imageCanvas.height
+        );
 
-  reset() {
-    form.reset();
-    Utils.editImage();
-    draw(img);
-  },
+        let imageData = ctx2.getImageData(0, 0, canvas.width, canvas.height);
+        ctx2.putImageData(imageData, 0, 0);
+        let dataURL = canvas.toDataURL('image/jpeg');
+        button.href = dataURL;
+    },
 
-  resetFilter() {
-    Utils.editImage();
-    draw(img);
-  }
+    // downloadCanvas() {
+    //     var imageCanvas = document.querySelector('.canvas');
+    //     let textCanvas = document.querySelector('.textCanvas');
+
+    //     var imageCanvasContext = imageCanvas.getContext('2d');
+    //     imageCanvasContext.drawImage(textCanvas, 69, 50);
+
+    //     // var dataURL = imageCanvas.toDataURL('image/png');
+    //     // var link = document.createElement('a');
+    //     // link.download = 'bottle-design.png';
+    //     // link.href = bottleCanvas
+    //     //     .toDataURL('image/png')
+    //     //     .replace('image/png', 'image/octet-stream');
+    //     // link.click();
+    // },
+
+    rotate(degrees) {
+        //   canvas = document.querySelector('.canvas');
+        let ctx = canvas.getContext('2d');
+
+        if (degrees == 90 || degrees == 270) {
+            canvas.width = img.height;
+            canvas.height = img.width;
+        } else {
+            canvas.width = img.width;
+            canvas.height = img.height;
+        }
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        if (degrees == 90 || degrees == 270) {
+            ctx.translate(img.height / 2, img.width / 2);
+        } else {
+            ctx.translate(img.width / 2, img.height / 2);
+        }
+        ctx.rotate((degrees * Math.PI) / 180);
+        ctx.drawImage(img, -img.width / 2, -img.height / 2);
+    },
+
+    aspectRatio(srcWidth, srcHeight, maxWidth, maxHeight) {
+        var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+        //   console.log('ratio is: ', ratio);
+        var newWidth = srcWidth * ratio;
+        var newHeight = srcHeight * ratio;
+        return {
+            width: newWidth,
+            height: newHeight
+        };
+    },
+
+    checkRgbSplit() {
+        if (checkRGB.checked === true) {
+            rgbButton.disabled = false;
+            rgbButton.classList.add('btn-active');
+        } else {
+            rgbButton.disabled = true;
+            rgbButton.classList.remove('btn-active');
+            draw(img);
+        }
+    },
+
+    checkInvertColors() {
+        if (checkInvert.checked === true) {
+            invertButton.disabled = false;
+            invertButton.classList.add('btn-active');
+        } else {
+            invertButton.disabled = true;
+            invertButton.classList.remove('btn-active');
+            draw(img);
+        }
+    },
+
+    removeText() {
+        if (document.querySelector('.textCanvas')) {
+            let textCanvas = document.querySelector('.textCanvas');
+            const context = textCanvas.getContext('2d');
+            context.clearRect(0, 0, textCanvas.width, textCanvas.height);
+
+            // textDiv.removeChild(textCanvas)
+            textCanvas.remove();
+        }
+        if (document.querySelector('.userText')) {
+            textArea.value = '';
+            textArea.remove();
+        }
+        if (document.querySelector('.temp-text')) {
+            let tempText = document.querySelector('.temp-text');
+            div.removeChild(tempText);
+        }
+        style = { top: 0, left: 0, width: 0, height: 0 };
+        textDiv.style = style;
+    },
+
+    reset() {
+        form.reset();
+        Utils.editImage();
+        draw(img);
+    },
+
+    resetFilter() {
+        Utils.editImage();
+        draw(img);
+    },
+
+    HSLToRGB(h,s,l) {
+        // Must be fractions of 1
+        s /= 100;
+        l /= 100;
+      
+        let c = (1 - Math.abs(2 * l - 1)) * s,
+            x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+            m = l - c/2,
+            r = 0,
+            g = 0,
+            b = 0;
+    
+            if (0 <= h && h < 60) {
+                r = c; g = x; b = 0;  
+              } else if (60 <= h && h < 120) {
+                r = x; g = c; b = 0;
+              } else if (120 <= h && h < 180) {
+                r = 0; g = c; b = x;
+              } else if (180 <= h && h < 240) {
+                r = 0; g = x; b = c;
+              } else if (240 <= h && h < 300) {
+                r = x; g = 0; b = c;
+              } else if (300 <= h && h < 360) {
+                r = c; g = 0; b = x;
+              }
+              r = Math.round((r + m) * 255);
+              g = Math.round((g + m) * 255);
+              b = Math.round((b + m) * 255);
+    
+              let rgb4 = [r,g,b]
+              return rgb4;
+      }
 };
